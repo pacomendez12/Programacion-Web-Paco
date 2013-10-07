5592a0f3705341e183bacaedea8bd804f4b1a7d0
Francisco Méndez <pacomendez12@hotmail.com>
2013-10-06 20:17:52 -0500

    responsivo básico

--- Proyecto/js/valida.js
+++ Proyecto/js/valida.js
@@ -1,30 +1,71 @@
 
 function validaFormularioRegistro() {
 	var form = document.getElementById("registro");
-		
+	expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	
 	if (validaNumero(form.codigo.value) == "") {
-		var msg = document.createTextNode('Ingresa el código');
 		var error = document.getElementById("error");
-		error.appendChild(msg);
+		document.getElementById("text-error").innerHTML = "Ingresa el código válido";
 		error.setAttribute('class', 'errorV');
 		form.codigo.focus();
 		setTimeout("error.setAttribute('class', 'errorI')", 3000);
 		return 0;
 	} else if (form.nombre.value == "") {
-		var msg = document.createTextNode('Ingresa un nombre');
 		var error = document.getElementById("error");
-		error.appendChild(msg);
+		document.getElementById("text-error").innerHTML = "Ingresa un nombre";
 		error.setAttribute('class', 'errorV');
-		form.codigo.focus();
+		form.nombre.focus();
 		setTimeout("error.setAttribute('class', 'errorI')", 3000);
 		return 0;
 	}
-
-
+	else if (form.ap.value == "") {
+		var error = document.getElementById("error");
+		document.getElementById("text-error").innerHTML = "Ingresa un apellido";
+		error.setAttribute('class', 'errorV');
+		form.ap.focus();
+		setTimeout("error.setAttribute('class', 'errorI')", 3000);
+		return 0;
+	}
+	else if (form.carrera.value == "") {
+		var error = document.getElementById("error");
+		document.getElementById("text-error").innerHTML = "Ingresa una carrera";
+		error.setAttribute('class', 'errorV');
+		form.carrera.focus();
+		setTimeout("error.setAttribute('class', 'errorI')", 3000);
+		return 0;
+	}
+	else if (!expr.test(form.correo.value	)) {
+		var error = document.getElementById("error");
+		document.getElementById("text-error").innerHTML = "Ingresa un email válido";
+		error.setAttribute('class', 'errorV');
+		form.correo.focus();
+		setTimeout("error.setAttribute('class', 'errorI')", 3000);
+		return 0;
+	}
 	else {
 		form.submit();
 	}
+}
+
+function cerrarError(){
+	error.setAttribute('class', 'errorI');
+}
+
+
+function activaCelular(){
+	var cel = document.getElementById('celular');
+	cel.disabled = !cel.disabled;
+}
+
+
+
+function oculta() {
+	var error = document.getElementById("error");
+	for(i = 100; i >=0  ; i--){
+		error.setAttribute('opacity',i);
+		setInterval("",100);
+	}
+	//error.setAttribute('class', 'errorI');
 }
 
 function ocultaError() {
