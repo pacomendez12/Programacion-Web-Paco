1654ddd3afe4cc5efe8e1dc0bb5c450d8f53cc4f
Francisco Méndez <pacomendez12@hotmail.com>
2013-10-16 16:09:07 -0500

    mvc primer commit

---
+++ Proyecto/controlador/alumnoCtl.php
@@ -0,0 +1,38 @@
+<?php
+	class AlumnoCtl{
+		/*atributos*/
+		public $modelo;
+		
+		/*constructor*/
+		function __construct() {
+			/*cargar modelo*/
+			require_once("modelo/alumnoMdl.php");
+			$this->modelo = new AlumnoMdl();
+		}
+	
+		function ejecutar() {
+			/*recibir acción*/
+			switch($_GET["acc"]) {
+				case "alta":
+					echo "<br>Debug: entró a alta en el controlador";
+					if(empty($_POST))
+						require_once("vista/registro.html");
+					 else {
+					 	/*obtener datos de POST*/
+					 	/*$codigo = $con->real_escape_string($_POST["codigo"]);*/
+					 	$codigo = $_POST["codigo"];
+					 	$nombre = $_POST["nombre"];
+					 	$apellidos = $_POST["apellido"];
+					 	
+						$resultado = $this->modelo->alta($codigo,$nombre,$apellidos);
+						if($resultado !== false){
+							require_once("vista/listaAlumnoView.html");
+						}else{
+							require_once("vista/error.html");
+						}
+					}
+				break;
+			}
+		}
+	}
+?>
